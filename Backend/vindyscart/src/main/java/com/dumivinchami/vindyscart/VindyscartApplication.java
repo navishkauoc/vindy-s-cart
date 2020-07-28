8135ed8b211e1b264ffdc72f7a3c29bcafd8e5d0
Navishka Kularathna <navishka.randika@gmail.com>
2020-07-29 00:24:05 +0530

    Backend initial commit

---
+++ Backend/vindyscart/src/main/java/com/dumivinchami/vindyscart/VindyscartApplication.java
@@ -0,0 +1,13 @@
+package com.dumivinchami.vindyscart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VindyscartApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VindyscartApplication.class, args);
+	}
+
+}
---
+++ Backend/vindyscart/src/test/java/com/dumivinchami/vindyscart/VindyscartApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dumivinchami.vindyscart;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VindyscartApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
