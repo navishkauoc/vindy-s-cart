faf2e1373481fb7a926ee5b29ce83ef205eb909e
Navishka Kularathna <navishka.randika@gmail.com>
2020-08-04 09:52:33 +0530

    Shopping cart and checkout added

---
+++ Frontend/HTML and CSS/app.js
@@ -0,0 +1,11 @@
+document.getElementById("to-cart-1").onclick = function() {changeText1()};
+
+function changeText1() {
+  document.getElementById("to-cart-1").innerHTML = "Added";
+}
+
+document.getElementById("to-cart-2").onclick = function() {changeText2()};
+
+function changeText2() {
+  document.getElementById("to-cart-2").innerHTML = "Added";
+}
